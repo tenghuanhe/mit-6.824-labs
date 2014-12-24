57af6f918492760acf88791a811a13dfd7e35c3b
Akshay Kumar <akshaykumar90@gmail.com>
2014-12-24 21:09:02 +0530

    Add MapReduce Part III: Handling worker failures

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -27,12 +27,19 @@ func (mr *MapReduce) KillWorkers() *list.List {
 }
 
 func (mr *MapReduce) RunMaster() *list.List {
-	waitingChannel := make(chan string)
+	waiting := make(chan string)
+	jobs := make(chan *DoJobArgs)
+	done := make(chan struct{})
 
 	workerDoJob := func(addr string, args *DoJobArgs) {
 		var reply DoJobReply
-		call(addr, "Worker.DoJob", args, &reply)
-		waitingChannel <- addr
+		ok := call(addr, "Worker.DoJob", args, &reply)
+		if ok {
+			done <- struct{}{}
+			waiting <- addr
+		} else {
+			jobs <- args
+		}
 	}
 
 	getNextWorkerAddr := func() string {
@@ -40,22 +47,42 @@ func (mr *MapReduce) RunMaster() *list.List {
 		select {
 		case addr = <-mr.registerChannel:
 			mr.Workers[addr] = &WorkerInfo{addr}
-		case addr = <-waitingChannel:
+		case addr = <-waiting:
 		}
 		return addr
 	}
 
+	// Scheduler
+	go func() {
+		for job := range jobs {
+			addr := getNextWorkerAddr()
+			go workerDoJob(addr, job)
+		}
+	}()
+
+	go func() {
+		for i := 0; i < mr.nMap; i++ {
+			args := &DoJobArgs{mr.file, Map, i, mr.nReduce}
+			jobs <- args
+		}
+	}()
+
 	for i := 0; i < mr.nMap; i++ {
-		args := &DoJobArgs{mr.file, Map, i, mr.nReduce}
-		addr := getNextWorkerAddr()
-		go workerDoJob(addr, args)
+		<-done
 	}
 
+	go func() {
+		for i := 0; i < mr.nReduce; i++ {
+			args := &DoJobArgs{mr.file, Reduce, i, mr.nMap}
+			jobs <- args
+		}
+	}()
+
 	for i := 0; i < mr.nReduce; i++ {
-		args := &DoJobArgs{mr.file, Reduce, i, mr.nMap}
-		addr := getNextWorkerAddr()
-		go workerDoJob(addr, args)
+		<-done
 	}
+
+	close(jobs)
 
 	return mr.KillWorkers()
 }
